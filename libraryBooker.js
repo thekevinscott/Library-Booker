af4918601c9080407dfda06a6104f5bf96451dc1
Kevin Scott <kevin@scottlabsllc.com>
2012-07-15 14:06:14 -0400

    Test getting the buttons

--- libraryBooker.js
+++ libraryBooker.js
@@ -2,7 +2,21 @@
 var libraryBooker;
 (function($){
    libraryBooker = function(){
-      console.log(window.location);
+      var debug = true;
+      var l = function(msg) {
+         if (debug && window['console']) { console.log(msg); }
+      }
+      var goodreads = function() {
+         var addMyBook = $('img[src=http://www.goodreads.com/assets/layout/addMyBookbutton.jpg]');
+         console.log(addMyBook);
+      };
+      
+
+      switch(window.location.hostname) {
+         case 'www.goodreads.com' :
+            goodreads();
+         break;
+      }
       return {
          
          
