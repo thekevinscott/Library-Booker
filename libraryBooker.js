1cd5d699e309a379dfdda9e4401fa0dcc8490d9f
Kevin Scott <kevin@scottlabsllc.com>
2012-07-15 14:36:04 -0400

    fix typo

--- libraryBooker.js
+++ libraryBooker.js
@@ -47,7 +47,7 @@ var libraryBooker;
                
                chrome.extension.sendRequest({
                   action: 'open_library',
-                  url : createLibraryUrl({library_url : library_url, book_author : book_author, book_title : book_title)
+                  url : createLibraryUrl({library_url : library_url, book_author : book_author, book_title : book_title})
                });
                
             });
