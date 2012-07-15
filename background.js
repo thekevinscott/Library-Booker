81ba434be428ffcb7938598dcac51a20f29b6c9f
Kevin Scott <kevin@scottlabsllc.com>
2012-07-15 14:31:11 -0400

    Receive messages in background page

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) 
+{
+
+	var sender_tab_id = sender.tab.id;
+	
+	switch(request.action)
+	{
+	   case 'open_library' :
+	      var url = request.url;
+	      var book_author = request.book_author;
+	      var book_title = request.book_title;
+	      
+	      chrome.tabs.create({url : url}, function(tab) {
+	         alert('new tab!');
+	      });
+	   break;
+	}
+
+});
